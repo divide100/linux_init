911ac596d40c32b905a242cd4e525eba15c233e3
David Magee <magee.david1994@gmail.com>
2015-12-20 19:20:01 -0500

    added setting to util

---
+++ home/bin/.utilrc.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+###############
+#    PORT     #
+###############
+
+PORT_HOMEDIR=`pwd`
+
+
+
+###############
+#     SSH     #
+###############
+
+SSH_HOMEDIR=`pwd`
+SSH_ADDRESS='104.131.111.146'
